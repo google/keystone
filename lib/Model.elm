e2d8ee74a702712211c57b53429b0141b2b998aa
Nick Pascucci <nickpascucci@google.com>
2016-12-20 15:16:51 -0500

    Add basic parsing support and tests.

--- lib/Model.elm
+++ lib/Model.elm
@@ -1,6 +1,29 @@
 module Model exposing (..)
 
 import Combine exposing (..)
+import Combine.Char exposing (..)
+import List exposing (concat)
+
+
+type alias Model =
+    List Declaration
+
+
+type alias BlockName =
+    String
+
+
+type Statement
+    = SFulfill String
+    | SRequire String
+    | SInstance String String
+    | SConnect String String String
+
+
+type Declaration
+    = DInterface String
+    | DComponent String (List Statement)
+    | DSystem String (List Statement)
 
 
 comment : Parser s String
@@ -8,7 +31,157 @@ comment =
     regex "--[^\n]*"
 
 
-type ModelElement
-    = Channel ModelElement ModelElement
-    | Component
-    | System List ModelElement
+skippables : Parser s (List String)
+skippables =
+    many (comment <|> whitespace)
+
+
+name : Parser s String
+name =
+    whitespace *> regex "[a-zA-Z0-9_-]+" <* whitespace
+
+
+decl : String -> Parser BlockName res -> Parser BlockName res
+decl s p =
+    let
+        blockStart _ =
+            whitespace
+                *> string s
+                *> name
+                <* string "is"
+                <* whitespace
+
+        captureName n =
+            putState n
+
+        blockEnd name =
+            whitespace
+                *> string "end"
+                *> whitespace
+                *> string name
+                <* string ";"
+    in
+        (blockStart s >>= captureName)
+            *> skippables
+            *> p
+            <* skippables
+            <* withState blockEnd
+
+
+fulfill : Parser s Statement
+fulfill =
+    SFulfill
+        <$> (string "fulfill"
+                *> whitespace
+                *> name
+                <* string ";"
+            )
+
+
+require : Parser s Statement
+require =
+    SRequire
+        <$> (string "require"
+                *> whitespace
+                *> name
+                <* string ";"
+            )
+
+
+component : Parser BlockName Declaration
+component =
+    let
+        build n =
+            -- TODO Skip comments
+            DComponent n <$> many (choice [ fulfill, require ])
+    in
+        decl "component" (withState build)
+
+
+instance : Parser s Statement
+instance =
+    SInstance
+        <$> (name <* (string ":" *> whitespace))
+        <*> (name <* string ";")
+
+
+connection : Parser s Statement
+connection =
+    SConnect
+        <$> (string "connect" *> name)
+        <*> (string "to" *> name)
+        <*> (string "via" *> name <* string ";")
+
+
+system : Parser BlockName Declaration
+system =
+    let
+        build n =
+            (DSystem n)
+                <$> many (skippables *> choice [ instance, connection ])
+    in
+        decl "system" (withState build)
+
+
+interface : Parser BlockName Declaration
+interface =
+    decl "interface" (withState (\n -> succeed (DInterface n)))
+
+
+block : Parser BlockName Declaration
+block =
+    skippables *> choice [ component, system, interface ]
+
+
+modelFile : Parser BlockName Model
+modelFile =
+    many block
+
+
+markdownModel : Parser BlockName Model
+markdownModel =
+    concat
+        <$> many
+                (manyTill anyChar (string "```keystone")
+                    *> manyTill block (whitespace *> string "```")
+                )
+
+
+formatError : List String -> InputStream -> String
+formatError ms stream =
+    let
+        location =
+            currentLocation stream
+
+        separator =
+            "|> "
+
+        expectationSeparator =
+            "\n  * "
+
+        separatorOffset =
+            String.length separator
+
+        padding =
+            location.column + separatorOffset + 2
+    in
+        "Parse error around line:\n\n"
+            ++ toString location.line
+            ++ separator
+            ++ location.source
+            ++ "\n"
+            ++ String.padLeft padding ' ' "^"
+            ++ "\nI expected one of the following:\n"
+            ++ expectationSeparator
+            ++ String.join expectationSeparator ms
+
+
+
+-- parse : String -> Result String Model
+-- parse s =
+--     case Combine.parse model s of
+--         Ok ( _, _, e ) ->
+--             Ok e
+--
+--         Err ( _, stream, ms ) ->
+--             Err <| formatError ms stream
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,8 +2,144 @@ module Tests exposing (all)
 
 import Test exposing (..)
 import Expect
+import Model exposing (..)
+import Combine exposing (..)
+import String
 
 
 all : Test
 all =
-    describe "keystone" []
+    describe "keystone" [ parsing ]
+
+
+isErr : Result s t -> Bool
+isErr r =
+    case r of
+        Ok _ ->
+            False
+
+        Err _ ->
+            True
+
+
+doParse : Parser BlockName t -> String -> Result String t
+doParse p i =
+    case runParser p "<test>" i of
+        Ok ( _, stream, result ) ->
+            Ok result
+
+        Err ( _, stream, errors ) ->
+            let
+                location =
+                    (currentLocation stream)
+            in
+                Err
+                    ((String.join ", " errors)
+                        ++ " at line "
+                        ++ toString location.line
+                        ++ ": "
+                        ++ location.source
+                    )
+
+
+parsing : Test
+parsing =
+    describe "parsing"
+        [ test "components can be parsed" <|
+            \() ->
+                Expect.equal (Ok (DComponent "TestComp" []))
+                    (doParse
+                        component
+                        "component TestComp is\nend TestComp;"
+                    )
+        , test "interfaces can be parsed" <|
+            \() ->
+                Expect.equal (Ok (DInterface "TestIface"))
+                    (doParse
+                        interface
+                        "interface TestIface is\nend TestIface;"
+                    )
+        , test "basic systems can be parsed" <|
+            \() ->
+                Expect.equal (Ok (DSystem "TestSys" []))
+                    (doParse
+                        system
+                        "system TestSys is\n\nend TestSys;"
+                    )
+        , test "composite systems can be parsed" <|
+            \() ->
+                Expect.equal
+                    (Ok
+                        (DSystem "TestSys"
+                            [ (SInstance "PartA" "TestComponentA")
+                            , (SInstance "PartB" "TestComponentB")
+                            , (SConnect "PartA" "PartB" "TestChan")
+                            ]
+                        )
+                    )
+                    (doParse
+                        system
+                        """system TestSys is
+                          PartA: TestComponentA;
+                          PartB: TestComponentB;
+                          connect PartA to PartB via TestChan;
+                        end TestSys;"""
+                    )
+        , test "blocks must start and end with the same name" <|
+            \() ->
+                (doParse system "system BadSys is\nend SysBad;")
+                    |> isErr
+                    |> Expect.true "Expected syntax error"
+        , test "comments are recognized" <|
+            \() ->
+                Expect.equal (Ok "endtest")
+                    (doParse (comment *> whitespace *> string "endtest")
+                        """-- I'm a comment!
+                           endtest"""
+                    )
+        , test "skippables are recognized" <|
+            \() ->
+                Expect.equal (Ok "endtest")
+                    (doParse (skippables *> string "endtest")
+                        """-- I'm a comment!
+
+
+                           endtest"""
+                    )
+        , test "comments are skipped in blocks" <|
+            \() ->
+                Expect.equal (Ok (DInterface "TestIface"))
+                    (doParse
+                        block
+                        """
+
+                        -- This is a comment!
+                        interface TestIface is
+
+                          -- This is an inline comment!
+                        end TestIface;
+
+
+                        """
+                    )
+        , test "models can be embedded in Markdown" <|
+            \() ->
+                Expect.equal (Ok ([ DInterface "TestIface" ]))
+                    (doParse
+                        markdownModel
+                        """
+                        # Markdown test
+                        This model is embedded in a Markdown file.
+
+                        ```keystone
+                        -- This is a comment!
+                        interface TestIface is
+
+                          -- This is an inline comment!
+                        end TestIface;
+                        ```
+
+                        This is the end of the model.
+                        """
+                    )
+        ]
